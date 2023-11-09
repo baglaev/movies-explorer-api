e7bd827c31911c96b4ee1f2070003652f25e7535
baglaev <bglvssh@yandex.ru>
2023-11-10 01:43:57 +0300

    links

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.get('/crash-test', () => {
 
 app.use(limiter);
 // app.options('*', cors(corsOptions));
-app.use(cors({ origin: ['https://api.bglvssh.diploma.nomoredomainsrocks.ru/', 'http://api.bglvssh.diploma.nomoredomainsrocks.ru/', 'http://localhost:3001'], credentials: true }));
+app.use(cors({ origin: ['https://bglvssh.diploma.nomoredomainsrocks.ru/', 'http://bglvssh.diploma.nomoredomainsrocks.ru/', 'http://localhost:3001'], credentials: true }));
 // app.use(cors());
 
 app.use(bodyParser.json());
